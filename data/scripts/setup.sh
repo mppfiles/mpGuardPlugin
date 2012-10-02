c8f6de7dc3a42408c56a47d5b1a0e787670bbf87
Martín Palacio Pentucci <mppfiles@gmail.com>
2012-10-01 21:27:05 -0300

    add php execution to script

--- data/scripts/setup.sh
+++ data/scripts/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building schema..."
-symfony propel:build-schema
+php symfony propel:build-schema
 cp plugins/mpGuardPlugin/config/schema.custom.yml config/schema.custom.yml
 
 echo "Building model..."
-symfony propel:build --all-classes
+php symfony propel:build --all-classes
 
 echo "Fixing model classes..."
 find lib/model -maxdepth 1 -name "sfGuard*" -not -name "*Query*" -exec sed -i "s/BasesfGuard/PluginsfGuard/g" '{}' \;
